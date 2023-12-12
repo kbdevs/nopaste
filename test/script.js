1326a05e7d606df0e496a325e80818d1070070ff
kb <86767129+kbdevs@users.noreply.github.com>
2023-12-11 20:19:00 -0800

    Create script.js

---
+++ test/script.js
@@ -0,0 +1,23 @@
+// Function to execute JavaScript fetched from URL
+function executeScriptFromURL(url) {
+  fetch(url)
+    .then(response => response.text())
+    .then(script => eval(script))
+    .catch(error => console.error('Error loading script:', error));
+}
+
+// Ask user for input
+var userInput = prompt('Enter an option (1 for editing):');
+
+if (userInput === '1') {
+  // Enable content editing
+  document.body.contentEditable = 'true';
+  document.designMode = 'on';
+} else if (userInput === 'sigma') {
+  // Replace 'your_js_file_url_here' with the actual URL of the JavaScript file you want to fetch and execute
+  var externalScriptURL = 'https://raw.githubusercontent.com/kbdevs/kbdevs/main/testing.js';
+  executeScriptFromURL(externalScriptURL);
+} else {
+  // Handling for other inputs or no input
+  alert('Invalid option or input.');
+}
