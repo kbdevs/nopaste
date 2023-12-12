e5ba72462a81a9ff63e7f11837ffc5f186bb722b
kb <86767129+kbdevs@users.noreply.github.com>
2023-12-11 20:19:42 -0800

    Create menu.js

---
+++ test/menu.js
@@ -0,0 +1,29 @@
+javascript:(function() {
+    function loadJSON(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.overrideMimeType("application/json");
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status == "200") {
+                callback(JSON.parse(xhr.responseText));
+            }
+        };
+        xhr.send(null);
+    }
+
+    function showOptionsAndExecute(jsonData) {
+        var options = jsonData.options;
+        var optionNames = Object.keys(options);
+        var selectedOption = prompt("Choose an option (Make sure you type it exactly): \n" + optionNames.join("\n"));
+
+        if (selectedOption && options[selectedOption]) {
+            var codeToExecute = options[selectedOption];
+            eval(codeToExecute);
+        } else {
+            alert("Invalid option selected or canceled.");
+        }
+    }
+
+    var jsonURL = 'https://raw.githubusercontent.com/kbdevs/school-stuff/main/data.json'; // Replace with your JSON file URL
+    loadJSON(jsonURL, showOptionsAndExecute);
+})();
