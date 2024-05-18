ccdef9c3ee6a26b5f1305ee50b9c3a625e4427c8
kb <86767129+kbdevs@users.noreply.github.com>
2024-05-18 09:33:41 -0700

    Delete test directory

--- test/aiwindow.js
+++
@@ -1,1 +0,0 @@
-javascript:(function(){var url='https://cdn.jsdelivr.net/gh/kbdevs/nopaste@latest/test/ai.html';var newTab=window.open();fetch(url).then(response=>response.text()).then(html=>newTab.document.write(html)).catch(error=>console.error('Error fetching HTML:',error));})();"
